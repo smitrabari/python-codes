57fe8769ba5d1c3d7ce7e7a415479b44c2513906
smitrabari <smitrabari18@gmail.com>
2026-03-01 20:05:57 +0530

    Typecasting Code

---
+++ Bro-code/Typecasting.py
@@ -0,0 +1,15 @@
+# Typecasting [ str(),int(),float(),bool() ]
+name = "Smit" # string
+age = 19 #integer
+gpa = 7.89 #float
+is_student = True #boolean
+
+# to view the data type of the variable
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+# Typecasting
+gpa = int(gpa) # float converted into integer
+print(gpa) # printed the integer value

49d2edbff1c3ca510d9d3d810ef99c2cb824dd48
smitrabari <smitrabari18@gmail.com>
2026-03-01 19:41:53 +0530

    added variable - boolean

--- Bro-code/Variables.py
+++ Bro-code/Variables.py
@@ -3,6 +3,7 @@ name = "Smit" # string
 lastname = 'Rabari'
 age = 19 #integer
 gpa = 7.89 #float
+is_student = True #boolean
 work_experience = 0
 print(name) #printing variable
 print(lastname)
@@ -13,3 +14,4 @@ print(f"My name is {name} and i am {lastname}") #format for printing a variable
 print(f"You are {age} years old")
 print(f"You are {work_experience} years old")
 print(f"my gpa is {gpa}")
+print(f"is student {is_student}")

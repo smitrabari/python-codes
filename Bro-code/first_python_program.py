222cccdbc1f2855a08b3fce9d32aecdfd5a94384
smitrabari <smitrabari18@gmail.com>
2026-03-01 15:38:34 +0530

    added comments

--- Bro-code/first_python_program.py
+++ Bro-code/first_python_program.py
@@ -1,4 +1,14 @@
 # this is my first program!!
+#^ this was a Comment!!
 print("Hello World")
 print('this is from python')
-
+'''
+this is
+multi line
+comment
+'''
+"""
+this is
+multi line
+comment
+"""

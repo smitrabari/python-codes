44dd2bbaa228fb15c717c63f58f499fd50ecf9f8
smitrabari <smitrabari18@gmail.com>
2026-03-01 16:05:11 +0530

    added variable - integer

--- Bro-code/Variables.py
+++ Bro-code/Variables.py
@@ -1,6 +1,12 @@
 # Variables - (string,integers,float and boolean)
 name = "Smit" # string
 lastname = 'Rabari'
+age = 19 #integer
+work_experience = 0
 print(name) #printing variable
 print(lastname)
+print(age)
+#printing with format!!
 print(f"My name is {name} and i am {lastname}") #format for printing a variable
+print(f"You are {age} years old")
+print(f"You are {work_experience} years old")

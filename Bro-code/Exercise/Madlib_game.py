50adcf1217b958b580241872d44b35e9b39a0383
smitrabari <smitrabari18@gmail.com>
2026-03-01 21:41:32 +0530

    Madlib Game

---
+++ Bro-code/Exercise/Madlib_game.py
@@ -0,0 +1,9 @@
+# madlib game
+# making story from random words
+# Noun - name of person, animal, place etc
+name = input("Enter Noun: ")
+# Adjective - information about a noun
+add = input("Enter Adjective: ")
+# Output and random story!!
+print(f"Today, I met {name}!!")
+print(f"He was {add}")

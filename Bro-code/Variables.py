7792a224f45bb87a629fecb9aa7d88c64200d68f
smitrabari <smitrabari18@gmail.com>
2026-03-01 15:46:46 +0530

    added variable - string

---
+++ Bro-code/Variables.py
@@ -0,0 +1,6 @@
+# Variables - (string,integers,float and boolean)
+name = "Smit" # string
+lastname = 'Rabari'
+print(name) #printing variable
+print(lastname)
+print(f"My name is {name} and i am {lastname}") #format for printing a variable

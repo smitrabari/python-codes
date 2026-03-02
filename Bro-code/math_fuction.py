09a1928eebf127d4c2b85b5e1e6034465dc9d542
smitrabari <smitrabari18@gmail.com>
2026-03-02 06:55:45 +0530

    Math Functions

---
+++ Bro-code/math_fuction.py
@@ -0,0 +1,14 @@
+# Math Functions
+x = 3.14
+y = -4
+z = 5
+result = round(x) # Round off the value of x.
+print(result)
+result = abs(y) # Absolute Value - Distance from 0 to number.
+print(result)
+result = pow(z,2) # Power the value to the base (base,power)
+print(result)
+result = max(x,y,z) # Maximum Value from input variables.
+print(result)
+result = min(x,y,z) # Minimum Value from input variables.
+print(result)
---
+++ Bro-code/math_import.py
@@ -0,0 +1,8 @@
+import math
+print(math.pi) # PI value
+print(math.e) # e value
+x = 9
+print(int(math.sqrt(x))) # Square root + Typecasting
+y = 9.9
+print(math.ceil(y)) # Round up
+print(math.floor(y)) # Round down

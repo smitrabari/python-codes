a11b3635368dcf772b5731bda24450ec5efba8aa
smitrabari <smitrabari18@gmail.com>
2026-03-03 18:00:40 +0530

    Strings indexing

---
+++ Bro-code/String_Indexing.py
@@ -0,0 +1,11 @@
+# String Indexing
+name = input("Enter your name: ")
+
+# [Start : End : Step]
+
+print(name[0]) # will print 1st element of string!!
+print(name[:4]) # will print till 4th element of string!!
+print(name[5:7]) # will print from 5th to 9th element of string!!
+print(name[5:]) # will start print from 5th element of string!!
+print(name[-1]) # will print last element of string!!
+print(name[::3]) # will print after every 3 element of string!!
